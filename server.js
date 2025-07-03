669fa26dcc5cc8754e4611bab5ecac9d13736623
Dylan Millson <dylanmillson@Dylans-MacBook-Air.local>
2025-07-03 23:04:38 +0200

    n

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = require('express');
-const path = require('path');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+import express from 'express';
+import path from 'path';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -52,4 +56,4 @@ process.on('SIGINT', () => {
   process.exit(0);
 });
 
-module.exports = app; 
+export default app; 
